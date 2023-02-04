078fb18cad39fb03450f70d8eabc390f81e24635
Cody De Arkland <codydearkland@gmail.com>
2023-02-04 00:14:36 -0800

    Fixing icon

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -11,9 +11,7 @@
   <form class="" on:submit|preventDefault="{signInWithGitHub}">
       <div class="">
         <!-- <input class="inputField bg-gray-900 border-b-4 border-b-blue-600 text-white w-full my-2" type="email" placeholder="Your email" bind:value="{email}" /> -->
-        <button type="submit" class="flex h-9 w-9 items-center justify-center rounded-lg  ring-yellow-400
-        transition-all hover:ring-2 " value="Send Link">
-            <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="36px" viewBox="0 0 20 20" width="36px" fill="#FFFFFF"><g><rect fill="none" height="20" width="20" x="0"/></g><g><path d="M10,4v1h6v10h-6v1h6c0.55,0,1-0.45,1-1V5c0-0.55-0.45-1-1-1H10z"/><polygon points="9.5,6.5 8.79,7.21 11.09,9.5 3,9.5 3,10.5 11.09,10.5 8.79,12.79 9.5,13.5 13,10"/></g>
-            </svg>
+        <button type="submit" class="flex h-9 w-9 items-center justify-center rounded-lg  ring-yellow-400 transition-all hover:ring-2" value="Send Link">
+            <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 9c2.7 0 5.8 1.29 6 2v1H6v-.99c.2-.72 3.3-2.01 6-2.01m0-11C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"/></svg>
         </button>
   </form>
